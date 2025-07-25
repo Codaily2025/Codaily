30426af99ba56eaa2676c7434888eafe413905fc
DESKTOP-DLOPF19\SSAFY <leee331@ajou.ac.kr>
2025-07-25 14:39:37 +0900

    feat: initialize Spring Boot project

---
+++ BE/src/main/java/com/codaily/CodailyApplication.java
@@ -0,0 +1,13 @@
+package com.codaily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodailyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodailyApplication.class, args);
+    }
+
+}
---
+++ BE/src/test/java/com/codaily/CodailyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codaily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodailyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
