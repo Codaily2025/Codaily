b0ff40a0c358228a552713ee11e1b9c15855560f
yeongenn <yngn0012@naver.com>
2025-08-05 13:24:16 +0900

    feat: 프로젝트 생성 페이지 Github 연동 영역 디자인 초안 구현

---
+++ FE/src/pages/ProjectCreate/ProjectCreateStep4.jsx
@@ -0,0 +1,11 @@
+import GithubConnection from '@/components/organisms/GithubConnection'
+
+const ProjectCreateStep4 = () => {
+    return (
+        <GithubConnection />
+
+    )
+
+}
+
+export default ProjectCreateStep4
