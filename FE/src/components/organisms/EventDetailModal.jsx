00d20a30779dc8a9be453fbc40922c2c874a39b7
yeongenn <yngn0012@naver.com>
2025-08-11 10:52:27 +0900

    style: 전체 일정 페이지 EventDetailModal 디자인 수정

--- FE/src/components/organisms/EventDetailModal.jsx
+++ FE/src/components/organisms/EventDetailModal.jsx
@@ -1,20 +1,41 @@
+import Button from '@/components/atoms/Button'
 import ModalOverlay from '../molecules/ModalOverlay'
 import ModalContainer from '../molecules/ModalContainer'
 import EventDetailContent from '../molecules/EventDetailContent'
-import CloseButton from '../atoms/CloseButton'
 
 const EventDetailModal = ({ data, onClose }) => {
-    const event = data?.event
-    console.log(event)
-    
+  const event = data?.event
+  console.log(event)
+
   if (!event) return null
+
+  const buttonStyle = {
+    backgroundColor: '#e9ecef',
+    color: '#6c757d',
+    marginTop: '60px',
+    padding: '12px 24px',
+    fontSize: '16px',
+    fontWeight: 500,
+    borderRadius: '12px',
+    width: '100%',
+    border: 'none',
+    cursor: 'pointer'
+  }
+
 
   return (
     <ModalOverlay onClick={onClose}>
       <div onClick={(e) => e.stopPropagation()}>
         <ModalContainer>
-          <EventDetailContent event={event} />
-          <CloseButton onClick={onClose} />
+          <EventDetailContent
+            event={event}
+          />
+          <Button
+            onClick={onClose}
+            style={buttonStyle}
+          >
+            닫기
+          </Button>
         </ModalContainer>
       </div>
     </ModalOverlay>
