8e922c9e06d91fa90783138cefc0fa10196293cf
haeuni <hyeun9313@gmail.com>
2025-08-11 12:56:54 +0900

    fix: 코드리뷰 제외

--- AI/src/code_review/previous/diff_file.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-class DiffFile(BaseModel):
-    filePath: str
-    patch: str
-    
--- AI/src/code_review/previous/full_file.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-class FullFile(BaseModel):
-    filePath: str
-    content: str
