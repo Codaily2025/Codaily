4e475628ffa2047f3f14b4fafd5dbaf9e6541126
서보혁 <sbh00092@gmail.com>
2025-08-12 23:09:02 +0900

    refactor: chat controller 명세서 부분 isReduced 전송 추가

--- BE/src/main/java/com/codaily/project/dto/FeatureSaveContent.java
+++ BE/src/main/java/com/codaily/project/dto/FeatureSaveContent.java
@@ -1,5 +1,6 @@
 package com.codaily.project.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 
@@ -11,6 +12,8 @@ public class FeatureSaveContent {
     private Long projectId;
     private Long specId;
     private String field;
+    @JsonProperty("isReduced")
+    private boolean isReduced;
     private FeatureSaveItem mainFeature;
     private List<FeatureSaveItem> subFeature;
 }
