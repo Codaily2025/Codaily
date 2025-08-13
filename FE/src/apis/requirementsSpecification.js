ed932643d37c113dd667c35702a9e017358364ec
oesara <osrosr888@naver.com>
2025-08-13 23:23:41 +0900

    feat: 요구사항 명세서 체크박스 토글 api 연결, field의 경우 하위 기능까지 잘 토글 처리되는지 확인 필요

--- FE/src/apis/requirementsSpecification.js
+++ FE/src/apis/requirementsSpecification.js
@@ -21,8 +21,40 @@ export async function downloadSpecDocument(projectId) {
     }
 }
 
-
-
+// 프로젝트 체크박스 선택, 해제 토글 패치
+// /api/projects/{projectId}/specification/reduce
+export async function toggleReduceFlag(projectId, field, featureId, isReduced) {
+    try {
+        if (field && featureId) {
+            throw new Error('field와 featureId는 둘 중 하나만 존재함. 둘 다 존재하면 안됨');
+        }
+        
+        const requestData = {
+            isReduced: isReduced, // 감축 여부
+        };
+        
+        let url = `projects/${projectId}/specification/reduce`;
+        const params = {};
+        
+        if (field) {
+            params.field = field;
+        } else if (featureId) {
+            params.featureId = featureId;
+        }
+        console.log('프로젝트 체크박스 토글 패치 요청:', params, requestData);
+        // console.log('요청 URL:', url);
+        // console.log('요청 파라미터:', params);
+        // console.log('요청 본문:', requestData);
+        const response = await authInstance.patch(url, requestData, {
+            params: params
+        });
+        
+        return response.data;
+    } catch (error) {
+        console.error('프로젝트 체크박스 선택, 해제 토글 패치 실패:', error);
+        throw error;
+    }
+}
 
 
 
